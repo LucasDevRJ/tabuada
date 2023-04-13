026fa93d087c680541c1b0d8092e8f42d40fe564
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-13 16:15:38 -0300

    Verificações do número digitado

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,13 +1,20 @@
 function calculaTabuada() {
 	var campoNumero = document.querySelector("input");
 	var tabuada = document.getElementById("tabuada").innerHTML;
+	var mensagem = document.getElementById("mensagem");
 
 	var numero = campoNumero.value;
 
-	for (var contador = 1; contador <= 10; contador++) {
-		var conta = numero * contador;
-		tabuada = tabuada + "<p>" + numero + " x " + contador + " = " + conta + "</p>";
-		document.getElementById("tabuada").innerHTML = tabuada;
+	if (numero.length == 0) {
+		mensagem.textContent = "Digite algum número!";
+	} else if (isNaN(numero)) {
+		mensagem.textContent = "Digite somente números!";
+	} else {
+			for (var contador = 1; contador <= 10; contador++) {
+			var conta = numero * contador;
+			tabuada = tabuada + "<p>" + numero + " x " + contador + " = " + conta + "</p>";
+			document.getElementById("tabuada").innerHTML = tabuada;
+		}
 	}
 }
 
