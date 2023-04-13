eb60572c56f77661c615226d472ddedc8a5bb0ce
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-13 15:26:02 -0300

    Lógica da tabuada concluída

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,10 +1,18 @@
 function calculaTabuada() {
 	var campoNumero = document.querySelector("input");
+	var tabuada = document.getElementById("tabuada").innerHTML;
 
 	var numero = campoNumero.value;
 
-	console.log(numero);
+	for (var antecessor = numero; antecessor >= 1; antecessor--) {
+		for (var contador = 1; contador <= 10; contador++) {
+			var conta = antecessor * contador;
+			tabuada = tabuada + "<p>" + antecessor + " x " + contador + " = " + conta;
+			document.getElementById("tabuada").innerHTML = tabuada;
+		}
+	}
 }
 
 var botao = document.querySelector("button");
 botao.addEventListener("click", calculaTabuada);
+
